c8b5a40739e6f921b8f39fd05d43f8f31e19c90c
techies-crobstacle <techies@crobstacle.com>
2026-03-19 03:22:09 +0530

    FIX

--- controllers/payment.js
+++ controllers/payment.js
@@ -472,7 +472,7 @@ async function handlePaymentSucceeded(paymentIntentId) {
       typeof order.shippingAddress === 'object' ? order.shippingAddress?.orderSummary : null;
 
     sendOrderConfirmationEmail(toEmail, toName, {
-      orderId:       order.id,
+      orderId:       order.displayId,
       totalAmount:   Number(order.totalAmount),
       itemCount:     allItems.length,
       products:      allItems.map((item) => ({
